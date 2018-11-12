2a72bda0e9d7bc851642334ff0f8ef154d327985
plty <platyunyu@gmail.com>
2018-11-12 22:08:01 +0800

    change sample data image

--- src/main/java/seedu/lostandfound/model/util/SampleDataUtil.java
+++ src/main/java/seedu/lostandfound/model/util/SampleDataUtil.java
@@ -21,18 +21,17 @@ public class SampleDataUtil {
     public static Article[] getUnresolvedSampleArticles() {
         return new Article[] {
             new Article(new Name("Nike Wallet"), new Phone("87438807"), new Email("alexyeoh@example.com"),
-                new Description("Found at Blk 30 Geylang Street 29"), Image.getDefault(), new Name("Alex Yeoh"),
-                    new Name("Not Claimed"), Boolean.FALSE,
-                getTagSet("Black")),
+                new Description("Found at Blk 30 Geylang Street 29"), new Image("data/images/3.jpeg"),
+                    new Name("Alex Yeoh"), new Name("Not Claimed"), Boolean.FALSE, getTagSet("Black")),
             new Article(new Name("Nike bag"), new Phone("99272758"), new Email("berniceyu@example.com"),
-                new Description("Found at Blk 30 Lorong 3 Serangoon Gardens"), Image.getDefault(),
+                new Description("Found at Blk 30 Lorong 3 Serangoon Gardens"), new Image("data/images/4.jpg"),
                     new Name("Bernice Yu"), new Name("Not Claimed"), Boolean.FALSE,
-                    getTagSet("Scratched", "Black")),
+                        getTagSet("Scratched", "Black")),
             new Article(new Name("Fitbit"), new Phone("93210283"), new Email("charlotte@example.com"),
-                new Description("Found at Blk 11 Ang Mo Kio Street 74"), Image.getDefault(),
+                new Description("Found at Blk 11 Ang Mo Kio Street 74"), new Image("data/images/5.jpg"),
                     new Name("Charlotte Oliveiro"), new Name("Not Claimed"), Boolean.FALSE, getTagSet("New")),
             new Article(new Name("Casio watch"), new Phone("91031282"), new Email("lidavid@example.com"),
-                new Description("Found at Blk 436 Serangoon Gardens Street 26"), Image.getDefault(),
+                new Description("Found at Blk 436 Serangoon Gardens Street 26"), new Image("data/images/6.png"),
                     new Name("David Li"), new Name("Not Claimed"), Boolean.FALSE, getTagSet("Red")),
         };
     }
@@ -40,10 +39,10 @@ public class SampleDataUtil {
     public static Article[] getResolvedSampleArticles() {
         return new Article[] {
             new Article(new Name("Bluetooth speaker"), new Phone("92492021"), new Email("irfan@example.com"),
-                        new Description("Found at Blk 47 Tampines Street 20"), Image.getDefault(),
+                        new Description("Found at Blk 47 Tampines Street 20"), new Image("data/images/8.jpg"),
                     new Name("Irfan Ibrahim"), new Name("Roy Balakrishnan"), Boolean.TRUE, getTagSet("Blue")),
             new Article(new Name("IPhone 7"), new Phone("92624417"), new Email("royb@example.com"),
-                    new Description("Found at Blk 45 Aljunied Street 85"), Image.getDefault(),
+                    new Description("Found at Blk 45 Aljunied Street 85"), new Image("data/images/7.jpg"),
                         new Name("Roy Balakrishnan"), new Name("Alex Yeoh"), Boolean.TRUE, getTagSet("Gold"))
         };
     }
